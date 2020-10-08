56ba2712f759e6104f61a2baa8bd7366e1949331
Emile Patry Blenkiron <emilepatryblenkiron@Emiles-MacBook-Pro.local>
2020-10-08 14:53:37 -0400

    Edit Landing

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,8 +6,8 @@ import me from '../../assets/images/me.jpg';
 const about_me = [
   <p>I help companies and organizations of all sizes build beautiful products through research-driven strategy,
   accessible and thoughtful design, and the constant pursuit of timeless visual principles.</p>,
-  <p>Today I'm focused on building <a className="highlight" href="https://www.hinterland.ai/" target="_blank" rel="noopener noreferrer">Hinterland</a>, SaaS for boutique travel agents, all while pursuing a Bachelor's Degree in Global Business and Digital Arts at the <a className="highlight" href="https://uwaterloo.ca/" target="_blank" rel="noopener noreferrer">University of Waterloo</a>. </p>,
-  <p>Actively seeking Summer 2021 <a className="highlight" href="https://www.linkedin.com/in/emilepatry/" target="_blank" rel="noopener noreferrer">internships!</a></p>,
+  <p>Today I'm focused on building <a className="highlight" href="https://www.hinterland.ai/" target="_blank" rel="noopener noreferrer">Hinterland</a>, creating delight for boutique travel agencies, all while pursuing a Bachelor's Degree in Global Business and Digital Arts at the <a className="highlight" href="https://uwaterloo.ca/" target="_blank" rel="noopener noreferrer">University of Waterloo</a>. </p>,
+  <h3>Actively seeking Summer 2021 <a className="highlight" href="https://www.linkedin.com/in/emilepatry/" target="_blank" rel="noopener noreferrer">internships!</a></h3>,
 ]
 
 const socials: {[social: string] : string} = {
