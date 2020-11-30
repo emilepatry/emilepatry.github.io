5550223c1121a36a639d70cdd96cdd2ec45c76a7
emilepatry <em.kiron@gmail.com>
2020-11-30 20:28:33 +0000

    Deploying to master from  @ cd946c84748489ab4b290ef7e3db0ab6c0b6a58c 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.268b140fd3577dc9354103373fb2232a.js"
+  "/precache-manifest.2807f7c5b1e8dd41241debc690a09f7f.js"
 );
 
 self.addEventListener('message', (event) => {
