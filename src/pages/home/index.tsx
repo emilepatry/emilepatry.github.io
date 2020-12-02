c04262fd442bce361ba948cb6ff44c9acb55e16e
Emile <em.kiron@gmail.com>
2020-12-02 10:53:37 -0500

    Sage Co link edit

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,7 +7,7 @@ const about_me = [
   <p>I help companies and organizations of all sizes build beautiful products through research-driven strategy,
   accessible and thoughtful design, and the constant pursuit of timeless visual principles.</p>,
   <p>Today I'm focused on building <a className="highlight" href="https://www.hinterland.ai/" target="_blank" rel="noopener noreferrer">Hinterland</a>, all while pursuing a Bachelor's Degree in Global Business and Digital Arts at the <a className="highlight" href="https://uwaterloo.ca/" target="_blank" rel="noopener noreferrer">University of Waterloo</a>. </p>,
-  <p>My past work includes redefining the traditional photoshoot at <a className="highlight" href="https://www.alluresystems.com/" target="_blank" rel="noopener noreferrer">Allure Systems</a>, creating websites for small businesses through <a className="highlight" href="https://www.thesage.co/" target="blank" rel="noopener norefferrer">Sage Co</a> and leading <a className="highlight" href="https://medium.com/waterloop/designing-waterloop-canadas-hyperloop-19fe8a4e0238" target="_blank" rel="noopener noreferrer">Canada's SpaceX Hyperloop</a> team.</p>,
+  <p>My past work includes redefining the traditional photoshoot at <a className="highlight" href="https://www.alluresystems.com/" target="_blank" rel="noopener noreferrer">Allure Systems</a>, creating websites for small businesses through <a className="highlight" href="https://thesage.co/#/" target="blank" rel="noopener norefferrer">Sage Co</a> and leading <a className="highlight" href="https://medium.com/waterloop/designing-waterloop-canadas-hyperloop-19fe8a4e0238" target="_blank" rel="noopener noreferrer">Canada's SpaceX Hyperloop</a> team.</p>,
 
 ]
 
